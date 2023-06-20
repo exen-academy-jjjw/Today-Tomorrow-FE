932e5471c15d3a8ac7143aef7305677166251e2f
jia-son <116625758+jia-son@users.noreply.github.com>
2023-06-20 16:51:28 +0900

    PrivateRoute 설정 추가 및 CategoryListPage컴포넌트 수정

--- src/components/post/CategoryListPage.js
+++ src/components/post/CategoryListPage.js
@@ -1,36 +1,43 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useLocation, useParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { fetchPostCategoryList, fetchPostList } from "../../modules/redux/listSlice.js";
+import {
+  fetchPostCategoryList,
+  fetchPostList,
+} from "../../modules/redux/listSlice.js";
 import { updateCompletion } from "../../modules/redux/postSlice.js";
 import { MdCheckBoxOutlineBlank, MdCheckBox } from "react-icons/md";
 import Header from "../header/Header.js";
 import "./css/listPageStyle.scss";
 
-import { MdFlightTakeoff, MdFavorite, MdMoreHoriz, MdOutlineLibraryAdd, MdOutlineModeEdit } from "react-icons/md";
+import {
+  MdFlightTakeoff,
+  MdFavorite,
+  MdMoreHoriz,
+  MdOutlineLibraryAdd,
+  MdOutlineModeEdit,
+} from "react-icons/md";
 
 const CategoryListPage = () => {
-
   const dispatch = useDispatch();
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
   const location = useLocation();
 
+  const params = useParams();
 
   useEffect(() => {
     async function fetchData() {
       try {
-        let response;
-        if (location.pathname === "/post/list/") {
-          response = await dispatch(fetchPostList());
-        } else {
-          const category = location.pathname.split("/post/list/")[1];
-          response = await dispatch(fetchPostCategoryList(category));
-        }
-
+        const category = params.category;
+        const response = await dispatch(fetchPostCategoryList(category));
         if (response.payload && Array.isArray(response.payload.data)) {
-          setData(response.payload.data);
+          const postData = response.payload.data.map((item) => ({
+            ...item,
+            completed: item.completion === 1,
+          }));
+          setData(postData);
         }
         setIsLoading(false);
       } catch (error) {
@@ -40,24 +47,30 @@ const CategoryListPage = () => {
     }
 
     fetchData();
-  }, [dispatch, location]);
+  }, [location]);
 
   const handleTitleClick = (postId) => {
     navigate(`/post/detail/${postId}`);
   };
 
-  const handleCheckboxClick = async (postId, completed) => {
+  const handleCheckboxClick = async (postId) => {
     try {
-      const updatedData = data.map((item) => {
-        if (item.postId === postId) {
-          const newCompleted = completed ? 0 : 1;
-          return { ...item, completed: newCompleted };
-        }
-        return item;
+      const completionValue = data.find((item) => item.postId === postId)
+        ?.completed
+        ? 0
+        : 1;
+      const res = await dispatch(
+        updateCompletion({ postId, completion: completionValue })
+      );
+      console.log(res);
+      setData((prevData) => {
+        return prevData.map((item) => {
+          if (item.postId === postId) {
+            return { ...item, completed: !item.completed };
+          }
+          return item;
+        });
       });
-
-      setData(updatedData);
-      await dispatch(updateCompletion({ postId, completion: updatedData.find((item) => item.postId === postId).completed }));
     } catch (error) {
       console.error(error);
     }
@@ -84,19 +97,31 @@ const CategoryListPage = () => {
       <div className="pageBg">
         <Header />
         <div className="categoryBox">
-          <button onClick={() => handleCategoryClick("travel")} className="categoryButton">
+          <button
+            onClick={() => handleCategoryClick("travel")}
+            className="categoryButton"
+          >
             <MdFlightTakeoff id="icon" />
             <span>travel</span>
           </button>
-          <button onClick={() => handleCategoryClick("hobby")} className="categoryButton">
+          <button
+            onClick={() => handleCategoryClick("hobby")}
+            className="categoryButton"
+          >
             <MdFavorite id="icon" />
             <span>hobby</span>
           </button>
-          <button onClick={() => handleCategoryClick("study")} className="categoryButton">
+          <button
+            onClick={() => handleCategoryClick("study")}
+            className="categoryButton"
+          >
             <MdOutlineModeEdit id="icon" />
             <span>study</span>
           </button>
-          <button onClick={() => handleCategoryClick("etc")} className="categoryButton">
+          <button
+            onClick={() => handleCategoryClick("etc")}
+            className="categoryButton"
+          >
             <MdMoreHoriz id="icon" />
             <span>etc</span>
           </button>
@@ -107,16 +132,25 @@ const CategoryListPage = () => {
         </div>
         <div>
           <ul style={{ listStyleType: "none" }}>
-            {data.map((item) => (
-              <li className="postList" key={item.postId}>
-                {item.completed ? (
-                  <MdCheckBox size={24} onClick={() => handleCheckboxClick(item.postId, item.completed)} />
-                ) : (
-                  <MdCheckBoxOutlineBlank size={24} onClick={() => handleCheckboxClick(item.postId, item.completed)} />
-                )}
-                <span onClick={() => handleTitleClick(item.postId)}>{item.title}</span>
-              </li>
-            ))}
+            {data &&
+              data.map((item) => (
+                <li className="postList" key={item.postId}>
+                  {item.completed ? (
+                    <MdCheckBox
+                      size={24}
+                      onClick={() => handleCheckboxClick(item.postId)}
+                    />
+                  ) : (
+                    <MdCheckBoxOutlineBlank
+                      size={24}
+                      onClick={() => handleCheckboxClick(item.postId)}
+                    />
+                  )}
+                  <span onClick={() => handleTitleClick(item.postId)}>
+                    {item.title}
+                  </span>
+                </li>
+              ))}
           </ul>
         </div>
         <div className="addBtn">
