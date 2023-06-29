4fcd3d9aeaa828458175ae9030203a05c915384a
jia-son <116625758+jia-son@users.noreply.github.com>
2023-06-29 22:30:08 +0900

    #route_1

--- src/App.js
+++ src/App.js
@@ -14,34 +14,47 @@ import Unregister from "./components/mypage/Unregister";
 import { PrivateRoute } from "./components/shared/PrivateRoute";
 import { PrivateRouteTwo } from "./components/shared/PrivateRouteTwo";
 
-import Review from './components/review/Review';
-import ReviewUpdate from './components/review/ReviewUpdate';
-import ReviewDetail from './components/review/ReviewDetail';
+import Review from "./components/review/Review";
+import ReviewUpdate from "./components/review/ReviewUpdate";
+import ReviewDetail from "./components/review/ReviewDetail";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/member/signup" element={<SignupPage />} />
-        <Route path="/member/login" element={<LoginPage />} />
         <Route path="/" element={<ExitBeforeMainin />} />
+
+        <Route path="/member">
+          <Route path="signup" element={<SignupPage />} />
+          <Route path="login" element={<LoginPage />} />
+        </Route>
 
         <Route element={<PrivateRouteTwo />}>
           <Route path="/main" element={<Main />} />
         </Route>
 
         <Route element={<PrivateRoute />}>
-          <Route path="/mypage" element={<Mypage />} />
-          <Route path="/mypage/unregister" element={<Unregister />} />
-          <Route path="/post/list" element={<ListPage />} />
-          <Route path="/post/detail/:postId" element={<PostDetailPage />} />
-          <Route path="/post/update/:postId" element={<PostUpdatePage />} />
-          <Route path="/post/create" element={<PostCreatePage />} />
-          <Route path="/post/list/:category" element={<CategoryListPage />} />
-          
-          <Route path='/review/create/:postId' element={<Review />} />
-          <Route path='/review/detail/:postId' element={<ReviewDetail />} />
-          <Route path='/review/update/:postId' element={<ReviewUpdate />} />
+          <Route path="/mypage">
+            <Route path="" element={<Mypage />} />
+            <Route path="unregister" element={<Unregister />} />
+          </Route>
+
+          <Route path="/post">
+            <Route path="create" element={<PostCreatePage />} />
+            <Route path="detail/:postId" element={<PostDetailPage />} />
+            <Route path="update/:postId" element={<PostUpdatePage />} />
+
+            <Route path="list">
+              <Route path="" element={<ListPage />} />
+              <Route path=":category" element={<CategoryListPage />} />
+            </Route>
+          </Route>
+
+          <Route path="/review">
+            <Route path="create/:postId" element={<Review />} />
+            <Route path="detail/:postId" element={<ReviewDetail />} />
+            <Route path="update/:postId" element={<ReviewUpdate />} />
+          </Route>
         </Route>
       </Routes>
     </>
